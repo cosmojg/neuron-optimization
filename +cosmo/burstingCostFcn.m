ce9e377294100199a257e4611ecf4a3ab56b94b8
cosmo <cosmo@cosmo.red>
2019-08-13 15:50:07 -0400

    Account for calcium concentration in cost function

---
+++ +cosmo/burstingCostFcn.m
@@ -0,0 +1,33 @@
+% this example simulation function simulates a model
+% and computes the burst period and mean spikes per burst
+% then, it returns a cost
+% if the burst period is within [950, 1050] ms, then that part of the cost is zero
+% if the mean spikes per burst is within [7, 10] then that part of the cost is zero
+% otherwise, the cost is the quadratic difference
+
+function [C, V] = burstingCostFcn(x,~)
+
+% x is a xolotl object
+x.reset;
+x.t_end = 10e3;
+x.approx_channels = 1;
+x.sim_dt = .1;
+x.dt = .1;
+x.closed_loop = true;
+
+x.integrate;
+V = x.integrate;
+
+% measure behaviour
+metrics = xtools.V2metrics(V,'sampling_rate',10);
+
+% accumulate errors
+C = xtools.binCost([950 1050],metrics.burst_period);
+C = C + xtools.binCost([.1 .3],metrics.duty_cycle_mean);
+C = C + xtools.binCost([7 10],metrics.n_spikes_per_burst_mean);
+C = C + xtools.binCost([111 136],x.AB.Ca_average);
+
+% safety -- if something goes wrong, return a large cost
+if isnan(C)
+	C = 1e3;
+end
---
+++ optimize_bursting.m
@@ -0,0 +1,59 @@
+% in this example script, we take a neuron
+% and optimize its maximal conductances so that it has a
+% desired period
+
+% first, we create our xolotl object
+x = xolotl.examples.BurstingNeuron('prefix','prinz');
+x.set('*gbar',veclib.shuffle((x.get('*gbar'))))
+
+% we instantiate the xfit object
+% and specify the particle swarm engine
+p = xfit('patternsearch');
+p.options.UseParallel = true;
+
+% we assign a cost function
+p.SimFcn = @cosmo.burstingCostFcn;
+
+
+% we assign the xolotl object
+p.x = x;
+
+% we optimzie over all maximal conductances
+p.parameter_names = x.find('*gbar');
+% lower bound values
+p.lb = [100 0 0 0 0 500 500];
+% upper bound values
+p.ub = [1e3 100 100 10 500 2000 2000];
+
+% display the results before optimization
+figure('outerposition',[300 300 1200 600],'PaperUnits','points','PaperSize',[1200 600]); hold on
+
+subplot(2,1,1); hold on
+set(gca,'XLim',[0 10],'YLim',[-80 50])
+x.t_end = 10e3;
+V = x.integrate;
+time = (1:length(V))*1e-3*x.dt;
+plot(time,V,'k')
+title('Before optimization')
+
+subplot(2,1,2); hold on
+set(gca,'XLim',[0 10],'YLim',[-80 50])
+
+
+% perform the optimization procedure
+p.fit;
+
+% the best fit is stored in p.seed
+% set the existing xolotl object to match
+x.set('*gbar',p.seed)
+
+
+% visualize the results of the optimization
+
+x.t_end = 10e3;
+V = x.integrate;
+time = (1:length(V))*1e-3*x.dt;
+plot(time,V,'r')
+title('After optimization')
+
+figlib.pretty('LineWidth', 1, 'PlotLineWidth', 1, 'PlotBuffer', 0)
